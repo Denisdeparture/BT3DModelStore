77f7078c04b48f0e7d6245b2c0bdce9545c7c913
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-24 12:58:14 +0300

    AddNewLayerDatabaseCommit
    Вынес базу данных в отельный слой

---
+++ Server/DataBase/MainDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DomainModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase.AppDbContexts
+{
+    public class MainDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public MainDbContext(DbContextOptions<MainDbContext> opt) : base(opt)
+        {
+            if (!Database.EnsureCreated())
+            {
+                Database.EnsureCreated();
+            }
+        }
+     
+    }
+        
+}

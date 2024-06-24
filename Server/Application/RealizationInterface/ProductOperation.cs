94da35fb6f83d6734d12c67825c8d2fe8f90b8b3
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-24 12:52:34 +0300

    Add Realization Iproductoperation from buisnes logic commit
    Добавил реализацию интерфейса из слоя бизнес логики

---
+++ Server/Application/RealizationInterface/ProductOperation.cs
@@ -0,0 +1,71 @@
+﻿using BuisnesLogic.Model;
+using BuisnesLogic.ServicesInterface;
+using AutoMapper;
+using Infrastructure;
+using DomainModel;
+using Application.Models;
+using DataBase.AppDbContexts;
+
+namespace Application.RealizationInterface
+{
+    public class ProductOperation : IProductOperation
+    {
+        private readonly MainDbContext _database;
+        private readonly IYandexClient _yandexClient;
+        private readonly IConfiguration _configuration;
+        public ProductOperation(MainDbContext database, IYandexClient yandexcloudoperation, IConfiguration configuration)
+        {
+            _database = database;
+            _yandexClient = yandexcloudoperation;
+            _configuration = configuration;
+        }
+        public async Task<string> CreateProductAsync(Product product, Stream model, string nameofobjectinstorage, string? pathfromstorage = null)
+        {
+            var res = await _yandexClient.AddModel(model, pathfromstorage ?? _configuration["PathFromStorage"]!, nameofobjectinstorage);
+            if (!res.isCorrect) throw new Exception();
+            product.Url = res.resultUrlFromModel!;
+            _database.Products.Add(product);
+            _database.SaveChanges();
+            return res.resultUrlFromModel!;
+        }
+
+        public async Task<bool> DeleteProductAsync(Product product, string nameofobjectinstorage, string? pathfromstorage = null)
+        {
+            var res = await _yandexClient.DeleteModel(pathfromstorage ?? _configuration["PathFromStorage"]!, nameofobjectinstorage);
+            if (res) throw new Exception();
+            var prod = _database.Products.Where(p =>
+            p.Name == product.Name &
+            p.Price == product.Price &
+            p.Description == product.Description).SingleOrDefault();
+            if (prod == null) throw new Exception();
+            _database.Remove(prod);
+            _database.SaveChanges();
+            return true;
+
+        }
+        public async Task<bool> DeleteProductAsync(string url, string nameofobjectinstorage, string? pathfromstorage = null)
+        {
+            var res = await _yandexClient.DeleteModel(pathfromstorage ?? _configuration["PathFromStorage"]!, nameofobjectinstorage);
+            if (res) throw new Exception();
+            var prod = _database.Products.Where(p =>
+            p.Url == url).SingleOrDefault();
+            if (prod == null) throw new Exception();
+            _database.Remove(prod);
+            _database.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<Product> GetAllProducts() => _database.Products;
+
+
+        public async Task<string> UpdateProductAsync(Product product, Stream model, string nameofobjectinstorage, string? pathfromstorage = null)
+        {
+            var res = await _yandexClient.UpdateModel(model, pathfromstorage ?? _configuration["PathFromStorage"]!, nameofobjectinstorage);
+            if (!res.isCorrect) throw new Exception();
+            _database.Update(product);
+            _database.SaveChanges();
+            return res.resultUrlFromModel!;
+        }
+    } 
+    
+}

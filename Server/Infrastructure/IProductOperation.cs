f92cc9222de9b0cedbbd89dc310ba4f75c174893
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-24 13:01:28 +0300

    AddNewInterfaceInInfrastructure
    реализация есть на клиенте

---
+++ Server/Infrastructure/IProductOperation.cs
@@ -0,0 +1,19 @@
+﻿using DomainModel;
+using Microsoft.AspNetCore.Routing.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public interface IProductOperation
+    {
+        public Task<string> CreateProductAsync(Product product, Stream model, string nameofobjectinstorage, string? pathfromstorage = null);
+        public Task<string> UpdateProductAsync(Product product, Stream model, string nameofobjectinstorage, string? pathfromstorage = null);
+        public Task<bool> DeleteProductAsync(string url, string nameofobjectinstorage, string? pathfromstorage = null);
+        public Task<bool> DeleteProductAsync(Product product, string nameofobjectinstorage, string? pathfromstorage = null);
+        public IEnumerable<Product> GetAllProducts();
+    }
+}

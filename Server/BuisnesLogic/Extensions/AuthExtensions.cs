9267e79927d70413179b5cc776136b8cf6ddb447
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-24 12:57:17 +0300

    ChangeBuisnesLogic
    Переместил JWTManager в бизнес логику

---
+++ Server/BuisnesLogic/Extensions/AuthExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BuisnesLogic.Extensions
+{
+    public static class AuthExtensions
+    {
+        public static SymmetricSecurityKey GetSymmetricSecurityKey(string sourcekey) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(sourcekey));
+
+    }
+}
---
+++ Server/BuisnesLogic/ServicesInterface/IYandexClient.cs
@@ -0,0 +1,16 @@
+﻿using BuisnesLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.ServicesInterface
+{
+    public interface IYandexClient
+    {
+        public Task<bool> DeleteModel(string postfix, string nameofmodel);
+        public Task<AwsActionResultModel> AddModel(Stream file, string postfix, string nameofmodel);
+        public Task<AwsActionResultModel> UpdateModel(Stream newfile, string postfix, string nameofmodel);
+    }
+}

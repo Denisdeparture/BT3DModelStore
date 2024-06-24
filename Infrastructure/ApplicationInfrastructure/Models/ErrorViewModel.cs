2b0a734b20eeb35496d5b0cab299e6a223cf2c99
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-24 12:32:12 +0300

    Change of structure Commit
    Я перенёс все слои архитектуры в папку server

--- Infrastructure/ApplicationInfrastructure/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

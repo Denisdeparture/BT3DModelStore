57a228a1a8d4a6d7d80191bd4b2d6e85bd9f9c1f
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-25 18:25:18 +0300

    AddStrategy
    Решил сделать собственную реализацию паттерна стратегия

---
+++ Server/BuisnesLogic/ServicesInterface/IStrategyConditions.cs
@@ -0,0 +1,14 @@
+﻿using BuisnesLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.ServicesInterface
+{
+    public interface IStrategyConditions
+    {
+        public StrategyResult<T> StrategyCondition<T>(IList<Predicate<T>> methodscondition, T obj);
+    }
+}

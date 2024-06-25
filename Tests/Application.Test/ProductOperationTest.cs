e0b1f5e5cf4d19e162da9bfa0740a20c2be86dfd
DenisIlimbaev <denis11you777602@gmail.com>
2024-06-25 18:20:09 +0300

    AddTestForAdminControllerAddFuctionalCommit
    Добавил тестирование контроллера по совместительству домашку

---
+++ Tests/Application.Test/ProductOperationTest.cs
@@ -0,0 +1,96 @@
+﻿using Application.Controllers;
+using Application.Models;
+using ApplicationInfrastructure;
+using BuisnesLogic.Model;
+using BuisnesLogic.ServicesInterface;
+using DataBase.AppDbContexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework.Internal;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+using Microsoft.Extensions.Logging;
+namespace Application.Tests
+{
+    public class ProductOperationTest
+    {
+
+#pragma warning disable NUnit1032 
+        private AdminController controller;
+        private IConfiguration Configuration;
+        private Mock<IYandexClient>? mockYandexClient;
+        private IStrategyConditions Strategy;
+        private MainDbContext data;
+        private Mock<ILogger<Program>> mocklogger;
+#pragma warning restore NUnit1032
+        [SetUp]
+        public void SetDi()
+        {
+
+            mockYandexClient = new Mock<IYandexClient>();
+            mocklogger = new Mock<ILogger<Program>>();
+            // test data p.s url is not exist
+            bool isCorrect = false;
+            string url = "";
+            using (var stream = new FileStream(@"C:\TestData\testfile.txt", FileMode.Open))
+            {
+                mockYandexClient.Setup(fakeobjfunc => fakeobjfunc.AddModel(stream, "", stream.Name)).Returns(new Task<AwsActionResultModel>(() => new AwsActionResultModel() { isCorrect = isCorrect, resultUrlFromModel = url }));
+            }
+            var initialization = new Dictionary<string, string>()
+            {
+                {"PathFromStorage", "Model"}
+            };
+            Configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(initialization!)
+                .Build();
+            Strategy = new StrategyConditions();
+            data = new MainDbContext("Server=PC;Database=ApplicationDataBase;Trusted_Connection=True;TrustServerCertificate=True;Connection Timeout=60");
+            controller = new AdminController(data, mockYandexClient.Object, Configuration, Strategy, mocklogger.Object);
+        }
+        [Test]
+        public void Test_Add_When_ModelNull()
+        {
+            // Arrange
+            var task = controller.AddProductPost(null);
+            task.Wait();
+            // Act
+            var res = task.Result;
+        }
+        [Test]
+        public void Test_Add_When_ModelParametNull()
+        {
+            // Arrange
+            var task = controller.AddProductPost(new ProductViewModel());
+            task.Wait();
+            // Act
+            var res = task.Result;
+        }
+        [Test]
+        public void Test_Add_When_ModelStringParametNull()
+        {
+            // Arrange
+            using (var stream = new FileStream(@"C:\TestData\testfile.txt", FileMode.Open))
+            {
+                var task = controller.AddProductPost(new ProductViewModel()
+                {
+                    Price = 12,
+                    Description = "",
+                    Name = string.Empty,
+                    Quantity = 15,
+                    Model = new FormFile(stream, stream.Length, stream.Length, stream.Name, stream.Name)
+                });
+                task.Wait();
+                // Act
+                var res = task.Result;
+            }
+        }
+        [Test]
+        public void Test_Delete()
+        {
+
+        }
+
+        //[Test]
+        //public void Test_Update() { }
+    }
+}

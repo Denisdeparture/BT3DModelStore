e3beb0ba829d302c0f160dd9a9e292dc0dfe2954
Denisdeparture <denis11you777602@gmail.com>
2024-07-15 14:39:33 +0300

    AddInterfacesFromWorkTokafkaClient
    Класс KafkaClient реализует их так что за место него можно создать RebbitMQClient ;)

---
+++ Server/BuisnesLogic/ServicesInterface/ClientsInterfaces/MessageBrokers/IProduceClient.cs
@@ -0,0 +1,14 @@
+﻿using BuisnesLogic.Model.ServiceResultModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.ServicesInterface.ClientsInterfaces.MessageBrokers
+{
+    public interface IProduceClient<TModel>
+    {
+        public Task<ProduceResultModel> Produce(TModel model, CancellationTokenSource cts, string? topic = null);
+    }
+}

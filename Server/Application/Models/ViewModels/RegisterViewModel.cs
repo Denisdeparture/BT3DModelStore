a68161d6ea38b94d129e2c17292f46854560f160
Denisdeparture <denis11you777602@gmail.com>
2024-07-15 13:49:37 +0300

    ChangeViewModelsCommit
    Убрал некоторый view заменив их контрактами

---
+++ Server/Application/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Ваше Имя")]
+        public string? Name { get; set; }
+        [Display(Name = "Ваша Фамилия")]
+        public string? LastName { get; set; }
+        [Display(Name = "Ваш Email")]
+        public string Mail { get; set; } = null!;
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string PasswordConfirm { get; set; }
+    }
+}

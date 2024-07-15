cb8e317dbebbe67d9886e40aaaa4695823659335
Denisdeparture <denis11you777602@gmail.com>
2024-07-15 14:26:46 +0300

    MinimalChangeFromBLModels
    Добавлена новая модель для удобного представления возвращённых данных с Продюссера

--- Server/BuisnesLogic/Model/ServiceResultModels/AwsActionResultModel.cs
+++ Server/BuisnesLogic/Model/ServiceResultModels/AwsActionResultModel.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BuisnesLogic.Model
+namespace BuisnesLogic.Model.ServiceResultModels
 {
     public class AwsActionResultModel
     {
-        public bool isCorrect {  get; set; }
+        public bool isCorrect { get; internal set; }
         public string resultUrlFromModel { get; set; } = null!;
     }
 }
---
+++ Server/BuisnesLogic/Model/ServiceResultModels/StrategyResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.Model.ServiceResultModels
+{
+    public class StrategyResult<T>
+    {
+        public T? Object { get; set; }
+        public bool Completed { get; internal set; }
+        public int NumberConditionCounter { get; internal set; }
+
+        public static implicit operator bool(StrategyResult<T> result) => result.Completed;
+    }
+}
+

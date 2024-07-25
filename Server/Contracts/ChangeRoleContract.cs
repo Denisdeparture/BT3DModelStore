c3b02115199a7614e5a3cf024d2fae9b9c00cc34
Denisdeparture <denis11you777602@gmail.com>
2024-07-25 09:14:56 +0300

    MinimalChangeInBL

---
+++ Server/Contracts/ChangeRoleContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class ChangeRoleContract
+    {
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+    }
+}

6e0aac8d1fd58a15d69fbf6e60d90c308429393e
Denisdeparture <denis11you777602@gmail.com>
2024-07-25 09:15:35 +0300

    AddDbFactory
    Пока только добавил

---
+++ Server/DataBase/MainDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace DataBase.AppDbContexts
+{
+    public class MainDbContextFactory : IDesignTimeDbContextFactory<MainDbContext>, IDbContextFactory<MainDbContext>
+    {
+        /// <param name="args">Is connection jsons files </param>
+        public MainDbContext CreateDbContext(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            foreach(var jsonfiles in args)
+            {
+                configurationBuilder.AddJsonFile(jsonfiles);
+            }
+            var configuration = configurationBuilder.Build();
+            var contextOptionsBuilder = new DbContextOptionsBuilder<MainDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new NullReferenceException("Connection string 'DefaultConnection' not found.");
+            contextOptionsBuilder.UseNpgsql(connectionString);
+            return new MainDbContext(contextOptionsBuilder.Options);
+        }
+
+        public MainDbContext CreateDbContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

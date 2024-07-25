b2a9b5d7179637ce83ac2ec00e4ee0198813a435
Denisdeparture <denis11you777602@gmail.com>
2024-07-25 09:22:48 +0300

    Tests

---
+++ Tests/BuisnesLogic.Tests/SecretsProviderManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.Tests
+{
+    public class SecretsProviderManagerTest
+    {
+        [Fact]
+        public void Test_Test()
+        {
+            var provider = new SecretsProviderManager();
+        }
+        // ещё нужно создать баскет
+    }
+}

6840708aa8d9f11d7b1bcf43dfae3f1c5d5ac9fd
Denisdeparture <denis11you777602@gmail.com>
2024-07-25 09:20:02 +0300

    MinimalChangeInInfrastructure

--- Server/Infrastructure/IDeleteUserOperation.cs
+++ Server/Infrastructure/IDeleteUserOperation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.ModelResult;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Infrastructure
 {
     public interface IDeleteUserOperation
     {
-        public Task<bool> Delete(string id);
+        public Task<UserOperationModel> Delete(string id);
     }
 }
--- Server/Infrastructure/IUserOperation.cs
+++ Server/Infrastructure/IUserOperation.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DomainModel;
+using Infrastructure.ModelResult;
 namespace Infrastructure
 {
     public interface IUserOperation
     {
-        public Task<bool> Register(User user, string? role = null);
-        public Task<bool> Update(string id, User newdatauser);
+        public Task<UserOperationModel> Register(User user);
+        public Task<UserOperationModel> Update(string id, User newdatauser);
         public IEnumerable<User> GetAllUsers();
     }
 }

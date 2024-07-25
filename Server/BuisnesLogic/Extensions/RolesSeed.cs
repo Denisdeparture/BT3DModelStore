f602b11c4a54ca1853d914bcf46d74c6be71780b
Denisdeparture <denis11you777602@gmail.com>
2024-07-25 09:06:29 +0300

    AddRoleSeed
    При запуске приложения всегда требуется установить роли, через hasdata не вариант

---
+++ Server/BuisnesLogic/Extensions/RolesSeed.cs
@@ -0,0 +1,19 @@
+﻿using BuisnesLogic.Model.Roles;
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebServer
+{
+    public static class RolesSeed
+    {
+        public static IApplicationBuilder UseSeedRoles(this IApplicationBuilder services,IServiceProvider provider)
+        {
+           var roleManager = provider.CreateScope().ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+           roleManager.CreateAsync(new IdentityRole() { Name = RolesType.User.ToString(), NormalizedName = RolesType.User.ToString().ToUpper() }).Wait();
+           roleManager.CreateAsync(new IdentityRole() { Name = RolesType.Admin.ToString(), NormalizedName = RolesType.Admin.ToString().ToUpper() }).Wait();
+           return services;
+        }
+    }
+}

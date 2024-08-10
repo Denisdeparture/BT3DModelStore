ba2dc8b32c9ac8e00b9f408bbe8be0bec114c22a
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:50:04 +0300

    MinimalChangeInDomain

---
+++ Server/DomainModel/Interface/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Enum;
+
+namespace DomainModel.Interface
+{
+    public interface IEntity
+    {
+        protected int Level { get; set; }
+        public virtual PrivacyLevel PrivacyLevel { get => (PrivacyLevel)Level; set => Level = (int)value; }
+    }
+
+}
---
+++ Server/DomainModel/Interface/IProductStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Enum;
+
+namespace DomainModel.Interface
+{
+    public interface IProductStatus
+    {
+        protected int Status { get; set; }
+        public virtual ProductStatus ProductStatus { get => (ProductStatus)Status; set => Status = (int)value; }
+        protected int PayStatus { get; set; }
+        public virtual PayStatus ProductPayStatus { get => (PayStatus)PayStatus; set => PayStatus = (int)value; }
+    }
+}

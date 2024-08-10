aab68c11cef436aeac1598474bd1e555bce6b52c
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:50:35 +0300

    AddModelsDirectoryInInfrastructure

---
+++ Server/Infrastructure/ModelResult/RoleOperationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ModelResult
+{
+    public class RoleOperationModel
+    {
+        public bool IsSucceced {  get; set; }
+        public IList<Exception> errors { get; set; }
+    }
+}

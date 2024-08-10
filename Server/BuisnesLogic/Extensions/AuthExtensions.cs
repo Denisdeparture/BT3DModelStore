d4aa1c650e5c43b284b8cccd26a76c912076fd1b
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:09:36 +0300

    MinimalChangeInExtens

--- Server/BuisnesLogic/Extensions/AuthExtensions.cs
+++ Server/BuisnesLogic/Extensions/AuthExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+﻿using BuisnesLogic.Service.Safety;
+using DomainModel.Entity;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -7,6 +10,22 @@ namespace BuisnesLogic.Extensions
     public static class AuthExtensions
     {
         public static SymmetricSecurityKey GetSymmetricSecurityKey(string sourcekey) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(sourcekey));
-
+        public async static Task<T?> FindByNameAsync<T>(this UserManager<T> userManager, string name, string email) where T : User
+        {
+            var res = await userManager.FindByEmailAsync(email);
+            if (res is null) return null;
+            return res.UserNickName.Equals(name) ? res : null;
+        }
+        public static (string passwordhash, string salt) EncryptPassword(this string password, uint amount_of_salt)
+        {
+            SaltPassword salt = new SaltPassword();
+            var enc = salt.Salt(password, amount_of_salt);
+            return new(enc.hash, enc.salt);
+        }
+        public static bool PasswordsIsEquals(this string password, string salt, string passwordHash)
+        {
+            SaltPassword saltpass = new SaltPassword();
+            return saltpass.Compare(password, salt, passwordHash);
+        }
     }
 }
---
+++ Server/BuisnesLogic/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using BuisnesLogic.Service.Clients;
+using BuisnesLogic.Service.Managers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddPantryStorage(this IConfigurationBuilder configurationBuilder, string token, string bucket)
+        {
+            using (ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole()))
+            {
+                ILogger<PantrySecretsProviderManager> loggerForConfig = factory.CreateLogger<PantrySecretsProviderManager>();
+                if (string.IsNullOrWhiteSpace(token) | string.IsNullOrWhiteSpace(bucket)) throw new ArgumentNullException("Token or bucket name was incorrect");
+                var source = new PantrySecretsProviderManager(token, bucket, loggerForConfig);
+                var res = source.GetData();
+                configurationBuilder.AddJsonStream(res);
+                return configurationBuilder;
+            }
+        }
+        public static string GetServerAddres(this IConfiguration configuration) => string.Format("{0}://{1}", configuration["ServerPath:Protocol"]!, configuration["ServerPath:Host"]);
+        public static string GetAlternativeServerAddres(this IConfiguration configuration) => string.Format("{0}://{1}", configuration["ServerPath:Protocol"]!, configuration["ServerPath:HostAlternative"]);
+    }
+}

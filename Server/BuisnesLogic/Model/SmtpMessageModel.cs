1eb0d25351ab267ce0de0182a0d9fd8950ca4cf7
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:10:39 +0300

    AddSmtpSystem
    Добавлен сервис для проверки почты пользователя

---
+++ Server/BuisnesLogic/Model/SmtpMessageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.Model
+{
+    public record SmtpMessageModel(string nameCompanyOrAdministration,string RecipientEmail, string SenderEmail, string provider, string domain_region, int port = 465, bool useSsl = true);
+   
+}

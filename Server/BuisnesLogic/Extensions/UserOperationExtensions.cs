3fa9bb912e40eaf37c553e06d6d198714abb6e34
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:08:13 +0300

    AddUserOpExtensions

---
+++ Server/BuisnesLogic/Extensions/UserOperationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure;
+using Infrastructure.ModelResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesLogic.Extensions
+{
+    public static class UserOperationExtensions
+    {
+        public static async Task<UserOperationModel> UpdateViaMailAsync(this IUserOperation userOperation, string oldemail, string newemail)
+        {
+            var user = userOperation.GetAllUsers().Where(u => u.Email == oldemail).SingleOrDefault();
+            var anotherUserHasThisEmail = userOperation.GetAllUsers().Where(u => u.Email == newemail).SingleOrDefault();
+            if (anotherUserHasThisEmail is not null) throw new Exception("Another user just had this email");
+            if (user is null) throw new NullReferenceException(nameof(user));
+            user.Email = newemail;
+            var res = await userOperation.UpdateAsync(user.Id, user);
+            return res;
+        }
+        public static async Task<UserOperationModel> UpdateViaTelephoneFromEmailAsync(this IUserOperation userOperation,string email, string telephone)
+        {
+            var user = userOperation.GetAllUsers().Where(u => u.Email == email).SingleOrDefault();
+            if (user is null) throw new NullReferenceException(nameof(user));
+            user.PhoneNumber = telephone;
+            var res = await userOperation.UpdateAsync(user.Id, user);
+            return res;
+        }
+    }
+}

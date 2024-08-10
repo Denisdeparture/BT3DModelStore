5160834650dd619e2c8639428881cd11a782572c
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 12:57:58 +0300

    AddPayModel

---
+++ Server/Application/Models/PayModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Models
+{
+    public class PayModel
+    {
+        public string notification_type { get; set; } = null!;
+        public int withdraw_amount { get; set; }
+        public DateTime datetime { get; set; }
+
+    }
+}

ce094e5f32d841bdbc5c63417308e765c7a4be7a
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:38:28 +0300

    AddSafetyEntity
    Добавлена сущность для определения важности информации

---
+++ Server/DomainModel/Enum/PrivacyLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Enum
+{
+    public enum PrivacyLevel
+    {
+        Important,
+        Medium,
+        None
+    }
+}

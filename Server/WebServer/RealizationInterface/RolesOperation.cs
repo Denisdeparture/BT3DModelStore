7ac8bb319feeb750941b82c5c299d5d137d49383
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:57:50 +0300

    ChangeInRolesOperation

--- Server/WebServer/RealizationInterface/RolesOperation.cs
+++ Server/WebServer/RealizationInterface/RolesOperation.cs
@@ -1,44 +1,81 @@
-﻿using Infrastructure;
+﻿using DomainModel.Entity;
+using Infrastructure;
+using Infrastructure.ModelResult;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
+using System.Data;
 
 namespace WebServer.RealizationInterface
 {
-    public class RolesOperation : IRoleOperation
+    public class RolesOperation : IRoleOperation, IUsersRolesOperation
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        
-        private readonly ILogger _logger;
-        public RolesOperation(RoleManager<IdentityRole> rolemanager, ILogger logger)
+        private readonly UserManager<User> _userManager;
+        public RolesOperation(RoleManager<IdentityRole> rolemanager, UserManager<User> userManager)
         {
             _roleManager = rolemanager;
-            _logger = logger;
+            _userManager = userManager;
         }
-        public async Task<bool> CreateRole(string rolename)
+        public async Task<RoleOperationModel> CreateRole(string rolename)
         {
+            var model = new RoleOperationModel() { IsSucceced = false, errors = new List<Exception>() };
             if (await _roleManager.FindByNameAsync(rolename) != null)
             {
-                _logger.LogDebug("This Role is exist");
-                return false;
+                model.errors.Add(new Exception("This Role is exist"));
+                return model;
             }
             var res = await _roleManager.CreateAsync(new IdentityRole() { Name = rolename });
-            _logger.LogError(string.Join(Environment.NewLine, res.Errors.Select(obj => obj.Description + Environment.NewLine + obj.Code).ToArray()));
-            return res.Succeeded;
+            if (!res.Succeeded)
+            {
+                string error = string.Join(Environment.NewLine, res.Errors.Select(obj => obj.Description + Environment.NewLine + obj.Code).ToArray());
+                model.errors.Add(new Exception(error));
+                return model;
+            }
+            model.IsSucceced = true;
+            return model;
         }
 
-        public async Task<bool> DeleteRole(string name)
+        public async Task<RoleOperationModel> DeleteRole(string name)
         {
+            var model = new RoleOperationModel() { IsSucceced = false, errors = new List<Exception>() };
             var role = await _roleManager.FindByNameAsync(name);
-            if (role == null)
+            if (role is null)
             {
-                _logger.LogDebug("Role isn`t exist");
-                return false;
+                string error = "Role isn`t exist";
+                model.errors.Add(new Exception(error));
+                return model;
             }
             var res = await _roleManager.DeleteAsync(role);
-            _logger.LogError(string.Join(Environment.NewLine, res.Errors.Select(obj => obj.Description + Environment.NewLine + obj.Code).ToArray()));
-            return res.Succeeded;
+            if (res.Succeeded)
+            {
+                string error = string.Join(Environment.NewLine, res.Errors.Select(obj => obj.Description + Environment.NewLine + obj.Code).ToArray());
+                model.errors.Add(new Exception(error));
+                return model;
+            }
+            model.IsSucceced = true;
+            return model;
+        }
+        public async Task<RoleOperationModel> AddRoleFromUserAsync(string email, string role)
+        {
+            var user  = await _userManager.FindByEmailAsync(email);
+            var model = new RoleOperationModel() { IsSucceced = false, errors = new List<Exception>() };
+            if (user is null)
+            {
+                string error = "user isn`t exist";
+                model.errors.Add(new Exception(error));
+                return model;
+            }
+            var task = _userManager.GetRolesAsync(user);
+            task.Wait();
+            var userHasThisRole = task.Result.Where(r => r.Equals(role)).FirstOrDefault();
+            model.IsSucceced = true;
+            if (string.IsNullOrEmpty(userHasThisRole))
+            {
+                await _userManager.AddToRoleAsync(user, role);
+                return model;
+            }
+            return model;
         }
 
-        
     }
 }

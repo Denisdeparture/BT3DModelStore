aa0662ee8752bad6a47f1da28bb8053ccb649d99
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 12:57:13 +0300

    AddConfirmEmailModel

---
+++ Server/Application/Models/ViewModels/ConfirmViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Models.ViewModels
+{
+    public class ConfirmViewModel
+    {
+        public string Code { get; set; } = null!;
+        public RegisterViewModel model { get; set; } = null!;
+    }
+}

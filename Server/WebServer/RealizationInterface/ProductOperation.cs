43adc7aa9a4381370e6957a29f105cbf3b640e86
Denisdeparture <denis11you777602@gmail.com>
2024-08-10 13:57:38 +0300

    ChangeInProductOperation

--- Server/WebServer/RealizationInterface/ProductOperation.cs
+++ Server/WebServer/RealizationInterface/ProductOperation.cs
@@ -1,8 +1,9 @@
 ﻿using BuisnesLogic.Model;
 using Infrastructure;
-using DomainModel;
 using DataBase.AppDbContexts;
 using BuisnesLogic.ServicesInterface.ClientsInterfaces;
+using DomainModel.Entity;
+using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
 
 namespace WebServer.RealizationInterface
 {
@@ -35,7 +36,7 @@ namespace WebServer.RealizationInterface
             p.Name == product.Name &
             p.Price == product.Price &
             p.Description == product.Description).SingleOrDefault();
-            if (prod == null) throw new Exception();
+            if (prod == null) throw new NullReferenceException(nameof(prod));
             _database.Remove(prod);
             _database.SaveChanges();
             return true;
@@ -43,19 +44,41 @@ namespace WebServer.RealizationInterface
         }
         public async Task<bool> DeleteProductAsync(string url, string nameofobjectinstorage)
         {
-            var res = await _yandexClient.DeleteModel(_configuration["PathFromStorage"]!, nameofobjectinstorage);
-            if (res) throw new Exception();
             var prod = _database.Products.Where(p =>
             p.Url == url).SingleOrDefault();
-            if (prod == null) throw new Exception();
+            if (prod is null) throw new NullReferenceException(nameof(prod));
+            var res = await _yandexClient.DeleteModel(_configuration["PathFromStorage"]!, nameofobjectinstorage);
+            if (res) throw new Exception();
             _database.Remove(prod);
             _database.SaveChanges();
             return true;
         }
 
+        public async Task DeleteProductAsync(int id, string? nameofobjectinstoragel = null)
+        {
+            var prod = GetById(id);
+            if (prod is null) throw new NullReferenceException(nameof(prod));
+            if(!string.IsNullOrWhiteSpace(nameofobjectinstoragel))
+            {
+                var res = await _yandexClient.DeleteModel(_configuration["PathFromStorage"]!, nameofobjectinstoragel);
+                if (res) throw new Exception(" Delete model was incorrect " + this.ToString());
+            }
+            _database.Remove(prod);
+            _database.SaveChanges();
+        }
+
         public IEnumerable<Product> GetAllProducts() => _database.Products;
 
-
+        public Product? GetById(int id)
+        {
+            var allProducts = GetAllProducts();
+            return allProducts.Where(pr => pr.Id == id).SingleOrDefault();
+        }
+        public IEnumerable<Product> GetByName(string name)
+        {
+            var allproducts = GetAllProducts();
+            return allproducts.Where(pr => pr.Name.Equals(name));
+        }
         public async Task<string> UpdateProductAsync(Product product, Stream model, string nameofobjectinstorage)
         {
             var res = await _yandexClient.UpdateModel(model, _configuration["PathFromStorage"]!, nameofobjectinstorage);
